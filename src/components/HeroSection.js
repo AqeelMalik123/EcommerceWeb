d5648887fbbd1e8a970b536db693ab7eada39d1a
unknown <maqeel191@gmail.com>
2024-08-28 11:01:16 -0700

    ServiceSection

---
+++ src/components/HeroSection.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function HeroSection() {
+  return (
+    <div>
+
+
+      <p>Welcome Hero</p>
+      <div>
+        Thapa Store
+      </div>
+      <div>
+        LOREFsdofjosdjfposjgisrdngfndsfgn ig gdsfi gsdf gsdf hgdshf 
+      </div>
+      <button>
+        Shop
+      </button>
+      <div>
+        image section
+      </div>
+    </div>
+  )
+}
